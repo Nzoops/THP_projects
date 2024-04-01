4114b443655f6ea5bbe32d079a0c324fb6bc01ca
nzoops <garry.enzo.m@gmail.com>
2024-04-01 16:25:21 +0100

    adding the exercices for the day

---
+++ Ruby_exercices/my_first_program.rb
@@ -0,0 +1,91 @@
+#my first program
+#puts "Hello, world!"
+
+# the second one
+#puts "Hello again"
+#puts "One more time"
+#puts "OK, enough"
+
+#to the moooon
+#2.times do
+#    puts +1
+#end
+
+#puts "Salut"
+
+# test add
+#count = 1
+#10.times do
+#    puts count +1
+#end
+
+#exo 5
+#puts "On va compter le nombre d'heures de travail à THP"
+#puts "Travail : #{10 * 5 * 11}"
+#puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#uts "Et en secondes ?"
+#puts 10 * 5 * 11 * 60 * 60
+#puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#puts 3 + 2 < 5 - 7
+#puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#puts "Ok, c'est faux alors !"
+#puts "C'est drôle ça, faisons-en plus :"
+#puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+#ex_06
+#number_of_hours_worked_per_day = 10
+#number_of_days_worked_per_week = 5
+#number_of_weeks_in_THP = 11
+#puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+#ex07
+#v1
+#puts "Bonjour, c'est quoi ton blase ?"
+#user_name = gets.chomp
+#puts user_name
+
+
+#puts "Bonjour, c'est quoi ton blase ?"
+#print "> "
+#user_name = gets.chomp
+#puts user_name
+
+#v3
+#user_name = gets.chomp
+#puts user_name
+
+#ex08
+#puts "hey homie, what's ur name?"
+#user_name = gets.chomp
+#puts "Bonjour, #{user_name}"
+
+#ex09
+#puts "Hey homie, what's your name?"
+#first_name = gets.chomp
+#puts "No, I meant your family name, of course I know your name"
+#second_name = gets.chomp
+#puts "your name is #{first_name} #{second_name}"
+
+#ex10
+#puts "when were you born?"
+#d_o_b = gets.chomp.to_i
+#puts "your age was #{2017-d_o_b} in 2017"
+
+#ex11
+#puts "how many time should I tell you sup?"
+#repeat = gets.chomp.to_i
+#repeat.times do
+#puts "Whazzaaaaa"
+#end
+
+#ex12
+puts "until how many should I count dude?"
+count = gets.chomp.to_i
+
+count.times do |iterate|
+    puts iterate +1
+end
